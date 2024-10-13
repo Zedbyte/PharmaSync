bbc0185604bbb54d619a6c62e6d73d97d1a728a9
Aetrix <markjeromesantos102@gmail.com>
2024-10-13 18:33:01 +0800

    changed md: to 2xl:

--- src/views/pages/login.php
+++ src/views/pages/login.php
@@ -11,11 +11,11 @@
 
     <script src="https://kit.fontawesome.com/c3d300ba56.js" crossorigin="anonymous"></script>
 </head>
-<body class="bg-white md:bg-[#F1F4F9]">
-    <main class="grid grid-cols-1 md:grid-cols-12 h-full">
+<body class="bg-white 2xl:bg-[#F1F4F9]">
+    <main class="grid grid-cols-1 2xl:grid-cols-12 h-full">
         <!-- Card Container (Form) -->
-        <div class="card__container w-full md:col-span-6 order-2 md:order-1">
-            <div class="card p-3 md:p-12 rounded-xl w-11/12 md:w-7/12">
+        <div class="card__container w-full 2xl:col-span-6 order-2 2xl:order-1">
+            <div class="card p-3 2xl:p-12 rounded-xl w-11/12 2xl:w-7/12">
                 <div class="header text-center pb-12">
                     <h1 class="text-3xl font-bold">Sign In</h1>
                     <p class="text-[#8D8D8D]">Welcome! Please enter your details.</p>
@@ -23,15 +23,17 @@
 
                 <form action="/login" method="POST" class="flex flex-col">
                     <div class="flex flex-col mb-7">
-                        <label class="input__box" for="email">Email</label>
+                        <label class="input__box font-semibold" for="email">Email</label>
                         <input type="email" name="email" placeholder="Email">
                     </div>
 
                     <div class="flex flex-col mb-2 relative">
-                        <label class="input__box" for="password">Password</label>
-                        <div>
+                        <label class="input__box font-semibold" for="password">Password</label>
+                        <div class="relative">
                             <input type="password" name="password" id="password" placeholder="Password" class="p-2 border border-gray-300 rounded w-full">
-                            <span id="togglePassword" class="eye__container absolute right-3 top-7 cursor-pointer disabled opacity-50">
+                            <span id="togglePassword" class="eye__container absolute 
+                            right-5 top-2 
+                            cursor-pointer disabled opacity-50">
                                 <i class="fa-solid fa-eye-slash"></i>
                             </span>
                         </div> 
@@ -51,7 +53,7 @@
 
                     <button type="submit" class="font-bold text-lg 
                     bg-[#3F70ED] hover:bg-[#5384ff] 
-                    ease-linear duration-200 text-white hover:text-[#c8d8fd] mb-6">Sign In</button>
+                    ease-linear duration-200 text-white hover:text-[#c8d8fd] mb-8">Sign In</button>
                 </form>
 
                 <div class="line__separator flex justify-between items-center"> 
@@ -60,7 +62,7 @@
                     <div class="line"></div> 
                 </div>
 
-                <div class="oauth__container flex justify-between mt-5">
+                <div class="oauth__container grid grid-cols-1 gap-5 place-items-center 2xl:flex 2xl:justify-evenly mt-5">
                     <div class="oauth__login flex">
                         <div class="oauth__icon">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
@@ -81,12 +83,17 @@
                         <span class="oauth__button__contents font-semibold">Sign in with Pfizer</span>
                     </div>
                 </div>
+
+                <section class="card__foot">
+                    <p>&copy; Group 9 Pharmaceutical Management System</p>
+                </section>
             </div>
         </div>
 
         <!-- Hero Container (Image) -->
-        <div class="hero__container order-1 md:order-2 hidden md:block md:col-span-6">
-            <img class="w-full h-48 md:h-auto object-cover" src="https://t3.ftcdn.net/jpg/01/06/33/74/360_F_106337464_PqrDoCy2HhtDr6ezOGq13hvpVFcMGpQV.jpg">
+        <div class="hero__container order-1 2xl:order-2 hidden 2xl:block 2xl:col-span-6">
+            <div class="img__container">
+            </div>
         </div>
     </main>
 
