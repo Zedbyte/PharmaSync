aa9f95daae9187a8aa5cbf4313fe074e7c2609fb
Aetrix <markjeromesantos102@gmail.com>
2024-10-23 14:47:57 +0800

    Update tailwind.config.js, navbar.css, sidebar.css root, navbar, and sidebar twigs
    - Added dark mode attributes for sidebar and navbar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,php,twig}", "./public/**/*.twig"],
+  darkMode: 'selector',
   theme: {
     extend: {
       transitionDuration: {
