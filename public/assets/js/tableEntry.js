dc8b63934ee6470332dbe0e520175d62756fdb2d
Aetrix <markjeromesantos102@gmail.com>
2024-11-09 11:27:49 +0800

    Updated tableEntry, theme JS and navbar twig.
    - Theme toggler translate x from 0 to 5
    - Updated svg color in theme js from indigo to violet
    - Commented out tableEntry JS and its link in purchase-list

--- public/assets/js/tableEntry.js
+++ public/assets/js/tableEntry.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const entryDropdownButton = document.getElementById('entryDropdownButton');
-    const entryDropdownMenu = document.getElementById('entryDropdownMenu');
-    const selectedEntry = document.getElementById('selectedEntry');
-    const customEntryInput = document.getElementById('customEntryInput');
-    const predefinedEntries = entryDropdownMenu.querySelectorAll('button[data-value]');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const entryDropdownButton = document.getElementById('entryDropdownButton');
+//     const entryDropdownMenu = document.getElementById('entryDropdownMenu');
+//     const selectedEntry = document.getElementById('selectedEntry');
+//     const customEntryInput = document.getElementById('customEntryInput');
+//     const predefinedEntries = entryDropdownMenu.querySelectorAll('button[data-value]');
 
-    predefinedEntries.forEach(entry => {
-        entry.addEventListener('click', () => {
-            const value = entry.getAttribute('data-value');
-            selectedEntry.textContent = `${value} entries`;
-            entryDropdownButton.dataset.selectedValue = value; // Attach selected value for MVC Controller
-        });
-    });
+//     predefinedEntries.forEach(entry => {
+//         entry.addEventListener('click', () => {
+//             const value = entry.getAttribute('data-value');
+//             selectedEntry.textContent = `${value} entries`;
+//             entryDropdownButton.dataset.selectedValue = value; // Attach selected value for MVC Controller
+//         });
+//     });
 
-    customEntryInput.addEventListener('change', () => {
-        const customValue = customEntryInput.value;
-        if (customValue) {
-            selectedEntry.textContent = `${customValue} entries`;
-            entryDropdownButton.dataset.selectedValue = customValue; // Attach custom value for MVC Controller
-        }
-    });
-});
+//     customEntryInput.addEventListener('change', () => {
+//         const customValue = customEntryInput.value;
+//         if (customValue) {
+//             selectedEntry.textContent = `${customValue} entries`;
+//             entryDropdownButton.dataset.selectedValue = customValue; // Attach custom value for MVC Controller
+//         }
+//     });
+// });
